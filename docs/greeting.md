e463d58efa0b7d4d3b4dee5b7b15d2afa5efa4cf
Firas RG <firasregayeg1220@yahoo.fr>
2023-07-23 02:40:08 +0100

    initial structure

---
+++ docs/greeting.md
@@ -0,0 +1,45 @@
+---
+title: Welcome
+slug: /
+description: Create a doc page with rich content.
+tags:
+- Getting started
+---
+
+# Welcome to OpenDevUnion!
+
+Your home 🏠 for collaborative learning, project development, and community growth.
+
+OpenDevUnion is a vibrant community of developers, students, and tech enthusiasts who come together to learn, share, and create. Our community thrives on the principles of collaboration, mutual respect, and continuous learning. Whether you're a seasoned developer or just starting your tech journey, we hope that you'll find this as a supportive environment to hone your skills and grow up.
+
+## What We Do
+- **Discussions**: We facilitate conversations on various tech topics, allowing members to share their insights, ask questions, and learn from each other.
+- **Learning**: We offer resources to help members at all levels of their learning journey.
+- **Projects**: Members can contribute to ongoing projects, start new ones, and learn from real-world development experiences.
+
+We believe that everyone has something to bring to the table, and there's always room for growth and improvement. That's why we've created this – to foster a spirit of learning, to collaborate, and to create something meaningful together.
+
+Ready to get started?
+
+## Getting Started
+OpenUnionDev is a [Discord community](https://discord.com/community)! 
+
+:::tip
+If you're not familiar with Discord, we recommend you to visit [the beginner's guide](https://support.discord.com/hc/en-us/articles/360045138571).
+:::
+
+To ensure a smooth onboarding experience, here are some steps you should take:
+
+1. **Join Us**: If you haven't already, make sure you've joined [our Discord community](https://discord.gg/4pKNAraH). Join discussions, ask questions, share your progress, and stay up-to-date with our news.
+
+2. **Familiarize Yourself With Server**: We've put together comprehensive explanations to guide you through our community's processes. Make sure to read guidelines closely.
+
+3. **Get a GitHub Account**: If you don't have a [GitHub account](https://docs.github.com/en/get-started/onboarding/getting-started-with-your-github-account) yet, now is the perfect time to create one! GitHub is the platform we use for our collaborative projects. 
+
+4. **Follow OpenDevUnion on GitHub**: Once you have your GitHub account set up, be sure to follow [our organization on GitHub](https://github.com/open-dev-union).
+
+5. **Learn About Our Bot**: We invite you to take a look at our [Discord Bot 🤖 project](/). It will provide you with an overview of the bot's functionality, the problems it aims to solve, and how you can join.
+
+Remember that our community is here to support you. Don't hesitate to reach out if you have questions or need help.
+
+Enjoy your stay !
---
+++ docusaurus.config.js
@@ -0,0 +1,126 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Open Dev Union',
+  tagline: 'Unleashing Potential Through Collaborative Learning',
+  favicon: 'imgs/favicon_io/favicon.ico',
+
+  // TODO - Set the production url of your site here
+  url: 'https://open-dev-union.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'open-dev-union', // Usually your GitHub org/user name.
+  projectName: 'open-dev-union-docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath:"/",
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      docs:{
+        sidebar: {
+          hideable: true,
+          autoCollapseCategories: true,
+        }
+      },
+      announcementBar: {
+        id: 'support_us',
+        content:
+            'We are looking for collaborators, <a target="_blank" rel="noopener noreferrer" href="https://discord.com/channels/1127563522163867739/1127567372438483074/1127584693446004777">join our community</a>',
+        backgroundColor: '#d0e3e3',
+        textColor: '#091E42',
+        isCloseable: true,
+      },
+      // TODO Replace with your project's social card
+      //image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        hideOnScroll: false,
+        title:"OpenDevUnion",
+        logo: {
+          alt: 'opu-img',
+          src: 'imgs/logo/on-white-removebg.png',
+          srcDark: 'imgs/logo/on-black-removebg.png',
+          width: 60
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Getting Started',
+          },
+          {
+            href: 'https://github.com/firasrg/open-dev-union',
+            label: 'Visit our GitHub',
+            position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'GitHub repository',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+            {
+                label: 'Getting Started',
+                to: '/docs/intro',
+            },
+            {
+                label: 'Discord',
+                href: 'https://discord.gg/mZhV4d36yK',
+            },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/firasrg/open-dev-union',
+            },
+        ],
+        // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
